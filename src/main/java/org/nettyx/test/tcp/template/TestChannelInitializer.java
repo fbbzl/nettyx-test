745f953a6d68f95212ce12c8ebab52d42b467f8c
fbbzl <290361638@qq.com>
2025-02-04 20:44:44 +0800

    build(test): 升级 nettyx 依赖至 2.3.22-RELEASE 版本
    - 更新 nettyx 依赖版本，以修复 EscapeCodec 类的问题
    - 修改 EscapeCodec 使用方式，以适应新版本的 API

--- src/main/java/org/nettyx/test/tcp/template/TestChannelInitializer.java
+++ src/main/java/org/nettyx/test/tcp/template/TestChannelInitializer.java
@@ -5,7 +5,6 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 import io.netty.handler.logging.LoggingHandler;
 import org.fz.nettyx.codec.EscapeCodec;
-import org.fz.nettyx.codec.EscapeCodec.EscapeMap;
 import org.fz.nettyx.codec.StartEndFlagFrameCodec;
 import org.fz.nettyx.handler.MessageEchoHandler;
 import org.nettyx.test.tcp.codec.UserCodec;
@@ -26,7 +25,7 @@ public class TestChannelInitializer<C extends Channel> extends ChannelInitialize
     protected void initChannel(C channel) {
         channel.pipeline().addLast(
                 new StartEndFlagFrameCodec(1024*1024, true, wrappedBuffer(new byte[]{ (byte) 0x7e }))
-                , new EscapeCodec(EscapeMap.mapHex("7e", "7d5e"))
+                , new EscapeCodec(EscapeCodec.EscapeMapping.mapHex("7e", "7d5e"))
                 , new UserCodec()
                 , new MessageEchoHandler()
                 , new LoggingHandler(HEX_DUMP));
