197145fe3fbf4e1fa7efed07d1e421e66bd6057a
fbbzl <290361638@qq.com>
2025-02-04 20:47:24 +0800

    refactor(test): 优化 TestChannelInitializer 类的代码结构
    -移除未使用的导入语句- 替换 static 导入为直接引用
    - 调整代码格式以提高可读性

--- src/main/java/org/nettyx/test/template/TestChannelInitializer.java
+++ src/main/java/org/nettyx/test/template/TestChannelInitializer.java
@@ -1,16 +1,15 @@
 package org.nettyx.test.template;
 
 
+import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
+import io.netty.handler.logging.ByteBufFormat;
 import io.netty.handler.logging.LoggingHandler;
 import org.fz.nettyx.codec.EscapeCodec;
 import org.fz.nettyx.codec.StartEndFlagFrameCodec;
 import org.fz.nettyx.handler.MessageEchoHandler;
 import org.nettyx.test.codec.UserCodec;
-
-import static io.netty.buffer.Unpooled.wrappedBuffer;
-import static io.netty.handler.logging.ByteBufFormat.HEX_DUMP;
 
 
 /**
@@ -24,10 +23,10 @@ public class TestChannelInitializer<C extends Channel> extends ChannelInitialize
     @Override
     protected void initChannel(C channel) {
         channel.pipeline().addLast(
-                new StartEndFlagFrameCodec(1024*1024, true, wrappedBuffer(new byte[]{ (byte) 0x7e }))
+                new StartEndFlagFrameCodec(1024*1024, true, Unpooled.wrappedBuffer(new byte[]{ (byte) 0x7e }))
                 , new EscapeCodec(EscapeCodec.EscapeMapping.mapHex("7e", "7d5e"))
                 , new UserCodec()
                 , new MessageEchoHandler()
-                , new LoggingHandler(HEX_DUMP));
+                , new LoggingHandler(ByteBufFormat.HEX_DUMP));
     }
 }
