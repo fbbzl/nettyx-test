55cbf1b833bb32eb1963caf6534c4f2c37b10bc6
fengbinbin <290361638@qq.com>
2025-04-11 14:18:04 +0800

    feat(test): create NettyxTestApplication and update dependencies
    - Create NettyxTestApplication class for testing- Update Nettyx version to2.3.24-RELEASE
    - Add Spring Boot and other dependencies for testing

---
+++ src/main/java/org/nettyx/test/NettyxTestApplication.java
@@ -0,0 +1,17 @@
+package org.nettyx.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fengbinbin
+ * @version 1.0
+ * @since 2025/4/11 14:16
+ */
+
+@SpringBootApplication
+public class NettyxTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NettyxTestApplication.class, args);
+    }
+}
