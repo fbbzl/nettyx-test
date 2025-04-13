aadbfb85bd75749bc970ea36af92d0b5bc4d7cd1
fengbinbin <29036163@qq.com>
2025-04-13 11:44:21 +0800

    build(deps): update dependency nettyx to v2.3.23-RELEASE
    - Update nettyx dependency from v2.3.22-RELEASE to v2.3.23-RELEASE
    - Simplify EscapeCodec instantiation by using static method import

--- src/main/java/org/nettyx/test/config/SerializerConfiguration.java
+++ src/main/java/org/nettyx/test/config/SerializerConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 public class SerializerConfiguration {
 
     @Bean
-    public StructSerializerContext StructSerializerContext() {
+    public StructSerializerContext structSerializerContext() {
         return new StructSerializerContext("org.nettyx");
     }
 }
