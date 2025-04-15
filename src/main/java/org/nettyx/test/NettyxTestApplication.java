856719d4641cc24c8d5d7563f6d4438c618e77f9
fengbinbin <29036163@qq.com>
2025-04-15 22:36:13 +0800

    build(deps): update dependency nettyx to v2.3.23-RELEASE
    - Update nettyx dependency from v2.3.22-RELEASE to v2.3.23-RELEASE
    - Simplify EscapeCodec instantiation by using static method import

--- src/main/java/org/nettyx/test/NettyxTestApplication.java
+++ src/main/java/org/nettyx/test/NettyxTestApplication.java
@@ -1,5 +1,6 @@
 package org.nettyx.test;
 
+import org.fz.nettyx.starter.annotation.EnableStructScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version 1.0
  * @since 2025/4/11 14:16
  */
-
+@EnableStructScan(basePackages = {"org.nettyx", "org"})
 @SpringBootApplication
 public class NettyxTestApplication {
 
