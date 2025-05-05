f09cda16d8510cdf08b87d3f2f00885eff0a9802
fengbinbin <29036163@qq.com>
2025-05-05 13:39:11 +0800

    perf(serializer): upgrade spring-boot-starter-nettyx and optimize serialization test- Upgrade

---
+++ src/test/java/codec/OutComplexStruct.java
@@ -0,0 +1,32 @@
+package codec;
+
+import lombok.Data;
+import org.fz.nettyx.serializer.struct.annotation.Struct;
+import org.fz.nettyx.serializer.struct.annotation.ToArray;
+import org.fz.nettyx.serializer.struct.basic.c.signed.Cchar;
+import org.fz.nettyx.serializer.struct.basic.c.signed.Cdouble;
+import org.fz.nettyx.serializer.struct.basic.c.signed.Cfloat;
+import org.fz.nettyx.serializer.struct.basic.c.stdint.signed.Cint32T;
+import org.fz.nettyx.serializer.struct.basic.c.stdint.unsigned.Cuint16T;
+import org.fz.nettyx.serializer.struct.basic.c.stdint.unsigned.Cuint8T;
+
+/**
+ * @author fengbinbin
+ * @version 1.0
+ * @since 2025/5/5 10:23
+ */
+
+@Data
+@Struct
+public class OutComplexStruct {
+    Cint32T  id;                // 4字节有符号整数
+    Cuint8T  flags;             // 1字节无符号整数
+    Cuint16T count;            // 2字节无符号整数
+    Cfloat   value;               // 4字节浮点数
+    @ToArray(length = 10)
+    Cchar[] name;             // 10字节字符串
+    Cdouble      bigValue;           // 8字节双精度浮点数
+    NestedStruct nested; // 嵌套结构体
+    @ToArray(length = 5)
+    Cuint8T[] array;
+}
