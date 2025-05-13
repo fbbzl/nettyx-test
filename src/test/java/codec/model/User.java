71e25336723208cb3196514752c550b036728318
fengbinbin <290361638@qq.com>
2025-05-14 07:43:05 +0800

    feat(test): finish springboot common test

--- src/test/java/codec/model/User.java
+++ src/test/java/codec/model/User.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import lombok.experimental.FieldDefaults;
 import org.fz.nettyx.serializer.struct.annotation.Struct;
 import org.fz.nettyx.serializer.struct.annotation.ToArray;
-import org.fz.nettyx.serializer.struct.annotation.ToArrayList;
 import org.fz.nettyx.serializer.struct.annotation.ToNamedEnum;
 import org.fz.nettyx.serializer.struct.basic.c.signed.*;
 import org.fz.nettyx.serializer.struct.basic.c.unsigned.*;
@@ -26,25 +25,25 @@ public class User<T, W, G> {
     clong4 uid;
     @ToArray(length = 5)
     cppushort[]     qqNames;
-    @ToArrayList(size = 5)
+    @ToArray(length = 5)
     List<cppushort> ss;
-    @ToArrayList(size = 5)
+    @ToArray(length = 5)
     List<cppushort> tts;
 
     Bom<T, W, G> b;
     @ToArray(length = 5)
     Bom<T, W, clong4>[]                         g111fs;
-    @ToArrayList(size = 5)
+    @ToArray(length = 5)
     List<Bom<T, Son<cuchar, Bill>, GirlFriend>> bs2d;
 
     T sonsbaba;
     @ToArray(length = 5)
     T[]     sonff;
-    @ToArrayList(size = 5)
+    @ToArray(length = 5)
     List<T> so111ns;
 
     W wwife;
-    @ToArrayList(size = 5)
+    @ToArray(length = 5)
     List<W> wives;
     @ToArray(length = 5)
     W[]     wives121212;
