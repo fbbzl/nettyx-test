5a6e3993f8f7c5757e3a3dbe986af18393ffbcbe
fengbinbin <290361638@qq.com>
2025-05-28 23:41:33 +0800

    feat(test): finish springboot common test

--- src/main/java/org/nettyx/test/serializer/SerializerTest.java
+++ src/main/java/org/nettyx/test/serializer/SerializerTest.java
@@ -31,31 +31,31 @@ import static org.fz.nettyx.serializer.struct.StructSerializer.toStruct;
 public class SerializerTest {
     static final TypeRefer<User<Bill, Wife<GirlFriend, Son<clong4, Bill>>, clong8>> userTypeRefer =
             new TypeRefer<User<Bill,
-                                Wife<GirlFriend, Son<clong4, Bill>>, clong8>>() {
+                    Wife<GirlFriend, Son<clong4, Bill>>, clong8>>() {
             };
 
-    static final Class<You> youCLass = You.class;
-    private static final StructSerializerContext context = new StructSerializerContext("org.nettyx");
+    static final         Class<You>              youCLass = You.class;
+    private static final StructSerializerContext context  = new StructSerializerContext("org.nettyx");
 
     @Before
     public void correctnessTest() {
-        TypeRefer< User< Bill,  Wife<Son<clong4, Bill>, Son<clong4, Bill>>, GirlFriend>> userTypeRefer =
-                new TypeRefer<>() {};
+        TypeRefer<User<Bill, Wife<Son<clong4, Bill>, Son<clong4, Bill>>, GirlFriend>> userTypeRefer =
+                new TypeRefer<User<Bill, Wife<Son<clong4, Bill>, Son<clong4, Bill>>, GirlFriend>>() { };
 
         byte[] bytes = new byte[1200];
         Arrays.fill(bytes, (byte) 67);
-        User struct   = toStruct(userTypeRefer, bytes);
-        byte[]           buf      = toBytes(userTypeRefer, struct);
-        byte[]           emptyBuf = toBytes(userTypeRefer, new User<>());
-        Score score = new Score();
+        User   struct   = toStruct(userTypeRefer, bytes);
+        byte[] buf      = toBytes(userTypeRefer, struct);
+        byte[] emptyBuf = toBytes(userTypeRefer, new User<>());
+        Score  score    = new Score();
         score.setMath(new cint(100));
         score.setEnglish(new cint(99));
         Contact contact = new Contact();
         contact.setPhone(new cint(78));
-        contact.setEmail(new cchar[]{new cchar(1), new cchar(4)});
+        contact.setEmail(new cchar[]{ new cchar(1), new cchar(4) });
         Student student = new Student();
         student.setId(new cint(23));
-        student.setName(new cchar[]{new cchar(7), new cchar(2)});
+        student.setName(new cchar[]{ new cchar(7), new cchar(2) });
         student.setScores(score);
         student.setContact(contact);
 
@@ -68,9 +68,9 @@ public class SerializerTest {
     public void testStructSerializer() {
         byte[] bytes = new byte[300];
         Arrays.fill(bytes, (byte) 67);
-        You turn = StructSerializer.toStruct(youCLass, bytes);
+        You    turn   = StructSerializer.toStruct(youCLass, bytes);
         byte[] bytes1 = StructSerializer.toBytes(turn);
-        System.err.println(bytes1.length+"个字节");
+        System.err.println(bytes1.length + "个字节");
         StopWatch stopWatch = StopWatch.create("反序列");
         stopWatch.start();
         for (int i = 0; i < 10_000_000; i++) {
